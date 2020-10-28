2466ed4bbad0bc3e7cb3be5736f1f4ef9b959b2b
Blessing Philips <blessingadesina@gmail.com>
2020-10-28 03:08:49 +0100

    implement feedback on re rendering

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ const App: React.FC = () => {
         setCurrentActions(actions)
         setPlaceholder(initialPlaceholder)
     });
-  });
+  }, []);
 
   const getCommands = (actions: ActionCategory[]) => {
     return actions
@@ -91,7 +91,7 @@ const App: React.FC = () => {
         action();
       });
     });
-  });
+  }, []);
 
   return (
     <div className="App">
